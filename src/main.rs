bcd0b37fb83c9534895c2923bec033bf66c08d5c
m01010100 <townsemc@sunypoly.edu>
2024-12-30 19:30:48 -0500

    control flow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    //let message = "Name: Matt, Height: ";
+    //message.clear();
+    let proceed = true;
+    if proceed {
+    println!("Proceeding");
+    } else {
+        println!("Not Proceeding")
+    }
+
+    let height = 75;
+    if height > 70 {
+        println!("Tall")
+    } else if height > 64 {
+        println!("Average")
+    }else {
+        println!("Short")
+    }
 }
