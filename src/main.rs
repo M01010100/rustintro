64403441c95e245da83c0044dfb98801c3415720
m01010100 <townsemc@sunypoly.edu>
2024-12-30 20:08:48 -0500

    Shadowing

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,20 @@ fn main() {
     } else {
         println!("Not Proceeding")
     }
+    let mut height: i32 = 75;
+    // ---- Shadowing height ----//
+    height = height - 15;
 
-    let height = 75;
-    if height > 70 {
+    let result = if height > 70 {
         println!("Tall")
     } else if height > 64 {
         println!("Average")
     }else {
         println!("Short")
-    }
+    };
+
+    println!("Result: {:#}", result);
+
+    let health = if height <180 {"good"} else{"unkown"};
+    println!("Health: {}", health)
 }
